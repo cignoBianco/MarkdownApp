01538335cce6f286cd321750f50b3b3df70be985
cignoBianco <smiana123@gmail.com>
2026-01-03 04:15:42 +0300

    Add TailwindCSS

--- MarkdownApp/src/renderer/src/main.tsx
+++ MarkdownApp/src/renderer/src/main.tsx
@@ -1,8 +1,12 @@
-import './assets/main.css'
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import './assets/main.css'
+
+// import './assets/base.css'
+
 import App from './App'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ MarkdownApp/vite.config.ts
@@ -0,0 +1,44 @@
+import { resolve } from 'path'
+import { defineConfig } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
+export default defineConfig({
+  main: {
+    build: {
+      externalizeDeps: true
+    },
+    resolve: {
+      alias: {
+        '@/lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared')
+      }
+    }
+  },
+  preload: {
+    build: {
+      externalizeDeps: true
+    }
+  },
+  renderer: {
+    assetsInclude: 'src/renderer/src/assets/**',
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'),
+        '@/hooks': resolve('src/renderer/src/hooks'),
+        '@/assets': resolve('src/renderer/src/assets'),
+        '@/store': resolve('src/renderer/src/store'),
+        '@/components': resolve('src/renderer/src/components'),
+        '@/mocks': resolve('src/renderer/src/mocks')
+      }
+    },
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()]
+      }
+    }
+  }
+})
+
